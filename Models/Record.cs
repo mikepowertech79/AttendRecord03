31d5f236662ceb8de4fff6561eaa4a3e8f36cdf2
winsermcr <winsermcr@gmail.com>
2021-12-16 15:32:06 +0800

    Add project files.

---
+++ Models/Record.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AttendRecord03.Models
+{
+    public class Record
+    {
+        public int Id { get; set; }
+
+        public string PersonEmail { get; set; }
+
+        public string PersonName { get; set; }
+
+        public string AbsenceType { get; set; }
+
+        public DateTime AbsenceTimeStart { get; set; }
+
+        public DateTime AbsenceTimeEnd { get; set; }
+
+        public int AbsenceHours { get; set; }
+
+
+
+        public Record()
+        {
+            
+        }
+
+
+
+    }
+}
