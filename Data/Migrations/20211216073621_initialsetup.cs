477a4d013c623d7e9df8cafd88e7a6e5c988629c
winsermcr <winsermcr@gmail.com>
2021-12-16 16:07:27 +0800

    PM> add-migration "initialsetup"
    PM> update-database

---
+++ Data/Migrations/20211216073621_initialsetup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AttendRecord03.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Record",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PersonEmail = table.Column<string>(nullable: true),
+                    PersonName = table.Column<string>(nullable: true),
+                    AbsenceType = table.Column<string>(nullable: true),
+                    AbsenceTimeStart = table.Column<DateTime>(nullable: false),
+                    AbsenceTimeEnd = table.Column<DateTime>(nullable: false),
+                    AbsenceHours = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Record", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Record");
+        }
+    }
+}
